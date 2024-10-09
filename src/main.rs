55fafefd6e769ca6857f1bc464e10cf56b66a89c
Joshua Willis <joshua.willis@intellitect.com>
2024-10-08 21:48:51 -0700

    Add dithering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::io::Cursor;
+
+use dither_lib::Resize;
+use image::{DynamicImage, ImageFormat, ImageReader, Rgb};
 use lambda_http::{service_fn, tracing, Body, Error, Request, RequestPayloadExt, Response};
-// use base64::{Engine as _, engine::general_purpose};
+use base64::{Engine as _, engine::general_purpose};
 // use serde_json::json;
 use s3::{S3Client, PutFile};
 
@@ -13,16 +17,33 @@ async fn handler<T: PutFile>(
         _ => return Ok(Response::builder().status(400).body("Invalid body".into()).unwrap()),
     };
 
-    client.put_file("s3-dithered-images", "test.png", "image/png", "base64", payload.clone()).await?;
+    client.put_file("s3-dithered-images", "originals/test.png", "image/png", "base64", payload.clone()).await?;
+
+    // let decoded = general_purpose::STANDARD.decode(&payload).expect("Failed to decode base64");
+    let decoded = payload;
+
+    let cursor = Cursor::new(decoded);
+    let image_reader = ImageReader::with_format(cursor, ImageFormat::Png);
+    let image = image_reader.decode().expect("Failed to decode image");
+    let dithered = dither_lib::DitherBuilder::new(image)
+        .highlights(Rgb([255;3]))
+        .shadows(Rgb([0;3]))
+        .resize(Resize::Scale(0.5))
+        .generate();
+
+    let mut dithered_bytes = Cursor::new(Vec::new());
+    dithered.write_to(&mut dithered_bytes, ImageFormat::Png).expect("Failed to write dithered image");
+    let dithered = dithered_bytes.into_inner();
+
+    client.put_file("s3-dithered-images", "dithered/test.png", "image/png", "base64", dithered.clone()).await?;
 
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body("Hello AWS Lambda HTTP request".into())
+        .body(format!("Uploaded!").into())
         .map_err(Box::new)?;
     Ok(resp)
 }
-
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
